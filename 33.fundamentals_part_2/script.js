d13f6fd85d5aa87a3a6b41ff18b8dee2f2692445
alessandro.maccario <alessandro.maccario@proton.me>
2026-05-17 15:33:05 +0200

    feat: extract data from an array

--- 33.fundamentals_part_2/script.js
+++ 33.fundamentals_part_2/script.js
@@ -1,1 +1,12 @@
 'use strict'; // activate strict mode: it has to be the very first statement of the script
+
+const jonas = {
+    firstName: "Jonas",
+    lastName: "Schmedtmann",
+    age: 2037 - 1991,
+    job: "teacher",
+    friends: ["Michael", "Peter", "Steven"]
+}
+
+console.log(jonas)
+console.log(`${jonas.firstName} has ${jonas["friends"].length} friends, and his best friend is called ${jonas.friends[0]}`);
