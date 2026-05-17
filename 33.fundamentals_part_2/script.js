ff03648bf9a6666eb8c3ce2d303c11cf5b4d4700
alessandro.maccario <alessandro.maccario@proton.me>
2026-05-17 16:32:39 +0200

    feat: add getSummary method to jonas object

--- 33.fundamentals_part_2/script.js
+++ 33.fundamentals_part_2/script.js
@@ -3,10 +3,20 @@
 const jonas = {
     firstName: "Jonas",
     lastName: "Schmedtmann",
-    age: 2037 - 1991,
+    birthYear: 1991,
     job: "teacher",
-    friends: ["Michael", "Peter", "Steven"]
-}
+    friends: ["Michael", "Peter", "Steven"],
+    hasDriverLicense: true,
+    calcAge: function () {
+        this.age = 2037 - this.birthYear;
+        return this.age
+    },
+    getSummary: function () {
+        // we need to call this.calcAge because otherwise, if it is not called, the age will be undefined unless the function is called before the getSummary method is called
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job} and has ${this.hasDriverLicense ? 'a' : 'not'} a driver's license.`
+    }
+};
 
 console.log(jonas)
-console.log(`${jonas.firstName} has ${jonas["friends"].length} friends, and his best friend is called ${jonas.friends[0]}`);
+console.log(`${jonas.firstName} has ${jonas["friends"].length} friends, and his best friend is called ${jonas.friends[0]} `);
+console.log(jonas.getSummary());
