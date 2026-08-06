5ddf1549599940a6a06fa6f3c4dafe5e898e2048
alessandro.maccario <alessandro.maccario@proton.me>
2026-08-07 00:48:08 +0200

    feat: start with implementation of game logic

--- 74.guess_my_number/script.js
+++ 74.guess_my_number/script.js
@@ -1,1 +1,13 @@
-"use strict";
+'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  // store the guessed value into the guess variable. Convert to Number as almost any user inputs will be a string, by default
+  const guess = Number(document.querySelector('.guess').value);
+
+  // check if the user inserted a value
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Missing value!';
+  } else {
+    console.log('Guess is ', guess);
+  }
+});
