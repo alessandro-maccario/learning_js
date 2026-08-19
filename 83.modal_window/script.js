b7806d07ff2fb0289e10c085c3e4741611c26899
alessandro.maccario <alessandro.maccario@proton.me>
2026-08-19 19:52:16 +0200

    initial commit for 83 modal window project

---
+++ 83.modal_window/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// select all the elements at the top of the file
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlaz');
+const btnCloseModal = document.querySelector('.close-modal');
+// collect all of the show modal class buttons
+const btnsShowModal = document.querySelectorAll('.show-modal');
