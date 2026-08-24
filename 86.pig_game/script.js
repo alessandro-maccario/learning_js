19e6da2b2e464dfbc630cae69ebd84d745f41f92
alessandro.maccario <alessandro.maccario@proton.me>
2026-08-24 18:45:34 +0200

    feat: if user rolls the dice the value gets added to the current score

--- 86.pig_game/script.js
+++ 86.pig_game/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // select elements
-const score0Element = document.querySelector('#score--0');
-const score1Element = document.querySelector('#score--1');
+let score0Element = document.querySelector('#score--0');
+let score1Element = document.querySelector('#score--1');
 const diceElement = document.querySelector('.dice');
 
 // starting conditions
@@ -20,23 +20,33 @@ let pseudoRandomNumberGenerator = function () {
   return Math.trunc(Math.random() * 6) + 1;
 };
 
-// game functionalities
+// GAME FUNCTIONALITIES
 // 1. USER ROLLS DICE
 document.querySelector('.btn--roll').addEventListener('click', function () {
   // store the random dice number generated into the randomDiceNumber variable
-  const randomDiceNumber = Number(document.querySelector('.guess').value);
+  let randomDiceNumber = pseudoRandomNumberGenerator();
+  console.log('random dice number: ', randomDiceNumber);
 
-  if (!randomDiceNumber === 1) {
+  // grab the current score for player 1
+  let currentScore = Number(document.getElementById('current--0').textContent);
+
+  if (randomDiceNumber !== 1) {
     // 1. add the random number to the score value of the player
     // 2. display the new score
-    // 3.
+    document.getElementById('current--0').textContent =
+      Number(currentScore) + Number(randomDiceNumber);
   } else {
     // if it is equal to 1, change player
   }
 });
 
 // 2. USER HOLDS DICE
-document.querySelector('.btn--hold').addEventListener('click', function () {});
+document.querySelector('.btn--hold').addEventListener('click', function () {
+  // add current score to total score
+  // if score >= 100 current player wins!
+});
 
 // 3. USER RESETS GAME
-document.querySelector('.btn--new').addEventListener('click', function () {});
+document.querySelector('.btn--new').addEventListener('click', function () {
+  // set all scores to 0 and set player1 as starting player
+});
