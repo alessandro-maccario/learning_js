6e099cb34dbb95470f9e335d092b389a855dae50
alessandro.maccario <alessandro.maccario@proton.me>
2026-08-25 21:18:12 +0200

    feat: add logic to switch player if the dice roll is 1 with counting new score dynamically

--- 86.pig_game/script.js
+++ 86.pig_game/script.js
@@ -3,6 +3,10 @@
 // select elements
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.querySelector('#score--1');
+const current0Element = document.getElementById('current--0');
+const current1ELement = document.getElementById('current--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 const diceElement = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,6 +16,10 @@ const btnHold = document.querySelector('.btn--hold');
 // set the initial condition of score elements to be 0
 score0Element.textContent = 0;
 score1Element.textContent = 0;
+// define current score for player 1 and player 2
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 // at the beginning of the game, add hidden class to the dice picture
 diceElement.classList.add('hidden');
 
@@ -28,25 +36,38 @@ let pseudoRandomNumberGenerator = function () {
 btnRoll.addEventListener('click', function () {
   // store the random dice number generated into the randomDiceNumber variable
   let randomDiceNumber = pseudoRandomNumberGenerator();
-  console.log('random dice number: ', randomDiceNumber);
 
-  // grab the current score for player 1
-  let currentScore0 = Number(document.getElementById('current--0').textContent);
-  // grab the current score for player 2
-  let currentScore1 = Number(document.getElementById('current--1').textContent);
+  // remove the hidden class to display the first dice
+  diceElement.classList.remove('hidden');
+  // display the dice face
+  document.querySelector('.dice').src = `dice-${randomDiceNumber}.png`;
 
   if (randomDiceNumber !== 1) {
     // 1. add the random number to the score value of the player
-    // 2. display the new score
-    document.getElementById('current--0').textContent =
-      Number(currentScore0) + Number(randomDiceNumber);
-
-    // 3. display the dice face
-    document.querySelector('.dice').src =
-      'dice' + '-' + String(randomDiceNumber) + '.png';
-    diceElement.classList.remove('hidden');
+    currentScore += randomDiceNumber;
+    // 2. display the new score dynamically based on the current active player
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    console.log(randomDiceNumber);
   } else {
-    // if it is equal to 1, change player
+    console.log(randomDiceNumber);
+    // switch player focus by first removing the player active class, then removing it
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+
+    // if the dice value is equal to 1, change player
+    document.querySelector('.dice').src = `dice-${randomDiceNumber}.png`;
+    // switch active player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    // reset the score for the new player before adding the new dice roll
+    currentScore = 0;
+    // display the new score dynamically based on the current active player
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--active');
   }
 });
 
